a0260340020b11a75bcdf653949e937314b27272
Luiz Rodrigues <lrodrigues@ifsp.edu.br>
2023-05-18 16:02:37 -0300

    ajuste da rota inicial

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const produtoController = require('../controllers/produto');
+
+router.get('/', produtoController.listarProdutos);
 
 module.exports = router;
